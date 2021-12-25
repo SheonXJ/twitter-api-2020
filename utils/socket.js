c071f35e61cf22ba71c4560d289c2f1fa7c76309
Tim <s87462830@gmail.com>
2021-12-25 16:40:11 +0800

    feat: add join public and leave public feature & modify onlineUser function

--- utils/socket.js
+++ utils/socket.js
@@ -19,47 +19,64 @@ const socket = server => {
   //socket.io 監聽器
   io.use(socketAuthenticated).on('connection', (socket) => {
     const currentUser = socket.user
-    console.log(`currentUser: ${socket.user.name}`)
 
-    // 使用者加入public Room
-    socket.on("public", async (data) => {
+    //使用者加入public Room
+    socket.on("public", async (roomName) => {
       //將目前使用者join pubic room
-      socket.join("public")
-      //將currentUser 存入 data array
-      io.emit('loginMsg', `${currentUser.name} has join the public Room`)
-      io.emit('loginUsers', data)
-      // 發送之前聊天紀錄
+      socket.join(roomName)
+      //檢查currentUser是否存在於onlineList
+      const checkList = onlineUsers.every(user => user.id !== currentUser.id)
+      if (checkList) {
+        onlineUsers.push(currentUser)
+      }
+      //發送目前上線的使用者
+      io.to(roomName).emit('loginUsers', onlineUsers)
+      //發送使用者進入房間的訊息
+      io.to(roomName).emit('loginMsg', `${currentUser.name} has join the ${roomName} Room`)
+      //發送之前聊天紀錄
       const allMessage = (await Message.findAll({ 
         raw:true,
-        nest:true, 
+        nest:true,
         where: {roomName: 'public'},
         include: {model: User, attributes: ['name', 'avatar']},
         order: [['createdAt', 'ASC']],
         limit: 50,
       }))
-      socket.emit("allMessage", allMessage)
+      io.to(roomName).emit("allMessage", allMessage)
     })
 
-    // 發送人數給前端輸出
-    io.emit("online", onlineCount) 
-
-    // 接收用戶傳送的訊息
+    //接收用戶傳送的訊息
     socket.on("sendMessage", async (msg) => {
+      //確保msg資料正確
       if (!msg.content || !msg.UserId || !msg.roomName) {
         return
       }
       const { content, UserId, roomName } = msg
+      //將使用者send的msg創建於資料庫 
       const sendMessage = (await Message.create({ content, UserId, roomName }))
+      //回傳整理後的msg給front-end
       msg.id = sendMessage.dataValues.id
       msg.avatar = currentUser.avatar
       msg.createdAt = sendMessage.dataValues.createdAt
-      io.to("public").emit("newMessage", msg)
+      if (msg.roomName === 'public') {
+        io.to(roomName).emit("newMessage", msg)
+      }
+    })
+
+    //接收使用者離開房間
+    socket.on("leave-room", (roomName) => {
+      socket.leave(roomName)
+      //發送使用者離開房間的訊息
+      io.to(roomName).emit('logoutMsg', `${currentUser.name} has leave the ${roomName} Room`)
+      //發送目前上線的使用者
+      onlineUsers = onlineUsers.filter(user => {
+        return user.id !== currentUser.id
+      })
+      io.to(roomName).emit('loginUsers', onlineUsers)
     })
 
     //當發生離線事件
     socket.on('disconnect', () => {
-      //有人離線時onlineCount -1
-      onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1
     });
   });
 
